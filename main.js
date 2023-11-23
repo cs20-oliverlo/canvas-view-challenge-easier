a656dbb1f40b84d907d3d05cbc61e6d0c4ed63e0
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2023-11-23 14:29:14 -0700

    done for real

--- main.js
+++ main.js
@@ -10,25 +10,25 @@ document.addEventListener("keyup", keyupHandler);
 
 function keydownHandler(event) {
     if (event.code === "ArrowUp") {
-        player[0].up = true;
+        player.up = true;
     }
     if (event.code === "ArrowLeft") {
-        player[0].left = true;
+        player.left = true;
     }
     if (event.code === "ArrowRight") {
-        player[0].right = true;
+        player.right = true;
     }
 }
 
 function keyupHandler(event) {
     if (event.code === "ArrowUp") {
-        player[0].up = false;
+        player.up = false;
     }
     if (event.code === "ArrowLeft") {
-        player[0].left = false;
+        player.left = false;
     }
     if (event.code === "ArrowRight") {
-        player[0].right = false;
+        player.right = false;
     }
 }
 
@@ -36,8 +36,8 @@ function keyupHandler(event) {
 
 // Reset Variables
 let walls;
-let triggers;
 let player;
+let camera;
 
 reset();
 
@@ -45,132 +45,103 @@ reset();
 // Animation
 requestAnimationFrame(animate);
 function animate() {
-    // Fill Background
     ctx.clearRect(0, 0, cnv.width, cnv. height);
 
-    // Food Helper Functions
+    // Wall Helper Functions
     for (let i = 0; i < walls.length; i++) {
         draw(walls, i);
-        checkCollision(i);
     }
 
     // Player Helper Functions
-    draw(player, 0);
+    draw(player);
     playerMovement();
+    checkCollision();
 
-    // Camera Movement Triggers
-    checkTriggers();
+    // Camera Helper Functions
+    cameraMovement();
     
     // Request Animation Frame
     requestAnimationFrame(animate);
 }
 
 function draw(shape, n) {
-    ctx.fillStyle = `${shape[n].color}`;
-    ctx.fillRect(shape[n].x, shape[n].y, shape[n].w, shape[n].h);
+    if (shape === walls) {
+        ctx.fillStyle = `${shape[n].color}`;
+        ctx.fillRect(shape[n].x - camera.x, shape[n].y - camera.y, shape[n].w, shape[n].h);
+    } else {
+        ctx.fillStyle = `${shape.color}`;
+        ctx.fillRect(shape.x - camera.x, shape.y - camera.y, shape.w, shape.h);
+    }
 }
 
 function playerMovement() {
-   playercontrols();
-   player[0].yV += player[0].yAccel;
-   player[0].y += player[0].yV;
+    playercontrols();
+    
+    // Move Player
+    player.yV += player.yAccel;
+    player.y += player.yV;
 
-    if (player[0].yV > 10) {
-        player[0].yV = 10;
-    } else if (player[0].yV < -10) {
-        player[0].yV = -10;
+    // Player Max Y velocity
+    if (player.yV > 10) {
+        player.yV = 10;
+    } else if (player.yV < -10) {
+        player.yV = -10;
     }
 }
 
 function playercontrols() {
-    if (player[0].up === true) {
-        player[0].yV = -10;
+    if (player.up === true) {
+        player.yV = -10;
     }
-    if (player[0].left === true) {
-        player[0].x -= player[0].xV;
+    if (player.left === true) {
+        player.x -= player.xV;
     }
-    if (player[0].right === true) {
-        player[0].x += player[0].xV;
+    if (player.right === true) {
+        player.x += player.xV;
     }
 }
 
-function checkCollision(n) {
+function checkCollision() {
     // Wall Detection
-    // Top Detection
-    if (player[0].y < walls[n].y + walls[n].h && player[0].y > walls[n].y && player[0].x + player[0].xV < walls[n].x + walls[n].w && player[0].x + player[0].w - player[0].xV > walls[n].x) {
-        player[0].y = walls[n].y - player[0].h;
-    }
-    // Bottom Detection
-    if (player[0].y + player[0].h > walls[n].y && player[0].y + player[0].h < walls[n].y + walls[n].h && player[0].x + player[0].xV < walls[n].x + walls[n].w && player[0].x + player[0].w - player[0].xV > walls[n].x) {
-        player[0].y = walls[n].y - player[0].h;
-        player[0].yV = 0;
-    }
-    // Left Detection
-    if (player[0].x < walls[n]. x + walls[n].w && player[0].x > walls[n].x && player[0].y < walls[n].y + walls[n].h && player[0].y + player[0].h > walls[n].y) {
-        player[0].x = walls[n].x + walls[n].w;
-    }
-    // Right Detection
-    if (player[0].x + player[0].w > walls[n]. x && player[0].x + player[0].w  < walls[n].x + walls[n].w && player[0].y < walls[n].y + walls[n].h && player[0].y + player[0].h > walls[n].y) {
-        player[0].x = walls[n].x - player[0].w;
+    for (let i = 0; i < walls.length; i++) {
+        // Top (of player)
+        if (player.y < walls[i].y + walls[i].h && player.y > walls[i].y && player.x + player.xV < walls[i].x + walls[i].w && player.x + player.w - player.xV > walls[i].x) {
+            player.y = walls[i].y - player.h;
+        }
+        // Bottom (of player)
+        if (player.y + player.h > walls[i].y && player.y + player.h < walls[i].y + walls[i].h && player.x + player.xV < walls[i].x + walls[i].w && player.x + player.w - player.xV > walls[i].x) {
+            player.y = walls[i].y - player.h;
+            player.yV = 0;
+        }
+        // Left (of player)
+        if (player.x < walls[i]. x + walls[i].w && player.x > walls[i].x && player.y < walls[i].y + walls[i].h && player.y + player.h > walls[i].y) {
+            player.x = walls[i].x + walls[i].w;
+        }
+        // Right (of player)
+        if (player.x + player.w > walls[i]. x && player.x + player.w  < walls[i].x + walls[i].w && player.y < walls[i].y + walls[i].h && player.y + player.h > walls[i].y) {
+            player.x = walls[i].x - player.w;
+        }
     }
 
-    // Canvas Borders
-    if (player[0].x < 0) {
-        player[0].x = 0;
+    // World Edge Borders
+    if (player.x < camera.x) {
+        player.x = camera.x;
     }
-    if (player[0].x + player[0].w > cnv.width) {
-        player[0].x = cnv.width - player[0].w;
+    if (player.x + player.w > camera.x + camera.w) {
+        player.x = camera.x + camera.w - player.w;
     }
 }
 
-function checkTriggers() {
-    for (let i = 0; i < triggers.length; i++) {
-        // Trigger Detection
-        if (player[0].x + player[0].w / 2 > triggers[i].x) {
-            triggers[i].state = true;
-        } else {
-            triggers[i].state = false;
-        }
-    }
+function cameraMovement() {
+    // Update Camera Position
+    camera.x = (player.x + player.w / 2) - camera.w / 2
 
-    if (triggers[1].state == true) {
-        // Stops the "camera" from moving past the end of the platform
-    } else if (triggers[0].state === true) {
-        if (player[0].x + player[0].w / 2 > cnv.width / 2) {
-            for (let i = 0; i < walls.length; i++) {
-                walls[i].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-            }
-            for (let i = 0; i < triggers.length; i++) {
-                triggers[i].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-            }
-            player[0].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-        }
-        if (player[0].x + player[0].w / 2 < cnv.width / 2) {
-            for (let i = 0; i < walls.length; i++) {
-                walls[i].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-            }
-            for (let i = 0; i < triggers.length; i++) {
-                triggers[i].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-            }
-            player[0].x += (cnv.width / 2) - (player[0].x + player[0].w / 2);
-        }
+    // Contrain Cameras X value
+    if (camera.x < 0) {
+        camera.x = 0;
+    } else if (camera.x + camera.w > walls[0].w) {
+        camera.x = walls[0].w - camera.w;
     }
-}
-
-function newPlayer(x1, y1, w1, h1, color1, up1, left1, right1, xV1, yV1, yAccel1) {
-    return {
-            x: x1,
-            y: y1,
-            w: w1,
-            h: h1,
-            color: color1,
-            up: up1,
-            left: left1,
-            right: right1,
-            xV: xV1,
-            yV: yV1,
-            yAccel: yAccel1
-    } 
 }
 
 function newWall(x1, y1, w1, h1, color1) {
@@ -183,31 +154,37 @@ function newWall(x1, y1, w1, h1, color1) {
     }
 }
 
-function newTrigger(x1, state1) {
-    return {
-        x: x1,
-        state: state1
-    }
-}
-
 function reset() {
     walls = [];
-    walls.push(newWall(0, cnv.height - 20, 1950, 20, "grey"));
+    walls.push(newWall(0, 460, 1950, 20, "grey"));
 
     let n = 520;
     for (let i = 0; i < 6; i++) {
         if (i % 2 === 0) {
-            walls.push(newWall(n, cnv.height / 2, 150, 20, "grey"));
+            walls.push(newWall(n, 240, 150, 20, "grey"));
         } else {
-            walls.push(newWall(n, (cnv.height / 2) + 100, 150, 20, "grey"));
+            walls.push(newWall(n, 240 + 100, 150, 20, "grey"));
         }
         n += 200;
     }
 
-    triggers = [];
-    triggers.push(newTrigger(cnv.width / 2, false));
-    triggers.push(newTrigger(1950 - cnv.width / 2, false));
+    player = {
+        x: 50,
+        y: 240,
+        w: 20,
+        h: 20,
+        color: "blue",
+        up: false,
+        left: false,
+        right: false,
+        xV: 5,
+        yV: 0,
+        yAccel: 1
+    };
 
-    player = [];
-    player.push(newPlayer(50, cnv.height / 2, 20, 20, "blue", false, false, false, 5, 0, 1));
+    camera = {
+        x: 0,
+        y: 0,
+        w: cnv.width
+    }
 }
